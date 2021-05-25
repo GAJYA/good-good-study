16680d8b3f70b6e8d0f6a489f43bf9377e928090
luoli_bcc <luoli_bcc@si-tech.com.cn>
2021-05-25 11:24:21 +0800

    reactivety

---
+++ .history/04-Vue/3-2-vue-dev/src/core/observer/dep_20210524104202.js
@@ -0,0 +1,70 @@
+/* @flow */
+
+import type Watcher from './watcher'
+import { remove } from '../util/index'
+import config from '../config'
+
+let uid = 0
+
+/**
+ * A dep is an observable that can have multiple
+ * directives subscribing to it.
+ */
+export default class Dep {
+  static target: ?Watcher;
+  id: number;
+  subs: Array<Watcher>;
+
+  constructor () {
+    this.id = uid++
+    this.subs = []
+  }
+
+  addSub (sub: Watcher) {
+    this.subs.push(sub)
+  }
+
+  removeSub (sub: Watcher) {
+    remove(this.subs, sub)
+  }
+
+  depend () {
+    if (Dep.target) {
+      Dep.target.addDep(this)
+    }
+  }
+
+  notify () {
+    // stabilize the subscriber list first
+    const subs = this.subs.slice()
+    if (process.env.NODE_ENV !== 'production' && !config.async) {
+      // subs aren't sorted in scheduler if not running async
+      // we need to sort them now to make sure they fire in correct
+      // order
+      subs.sort((a, b) => a.id - b.id)
+    }
+    for (let i = 0, l = subs.length; i < l; i++) {
+      subs[i].update()
+    }
+  }
+}
+// Dep.target用来存放目前正在使用的watcher
+// 全局唯一，并且一次也只能有一个watcher被使用
+// The current target watcher being evaluated.
+// This is globally unique because only one watcher
+// can be evaluated at a time.
+Dep.target = null
+const targetStack = []
+// 入栈并将当前watcher赋值给Dep.target
+// 在vue2以后，每一个组件对应一个watcher对象，每个组件都有mountComponent，在mountComponent中创建了watcher对象
+// 如果组件有嵌套，会先渲染子组件，此时父组件被挂起来了
+export function pushTarget (target: ?Watcher) {
+  targetStack.push(target)
+  Dep.target = target
+}
+
+export function popTarget () {
+  // 出栈操作
+  targetStack.pop()
+  Dep.target = targetStack[targetStack.length - 1]
+}

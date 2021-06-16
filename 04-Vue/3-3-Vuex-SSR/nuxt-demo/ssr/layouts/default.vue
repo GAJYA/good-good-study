3ba895b273bd3f4253a0e981a0cc637b6290aad3
十三月 <lunarjan@shisanyuedeMacBook-Pro.local>
2021-06-16 23:46:24 +0800

    nuxt-demo

---
+++ 04-Vue/3-3-Vuex-SSR/nuxt-demo/ssr/layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+      <ul>
+          <li>
+              <NuxtLink to="/">home</NuxtLink>
+          </li>
+          <li>
+              <NuxtLink to="/about">about</NuxtLink>
+          </li>
+      </ul>
+      <Nuxt/>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ 04-Vue/3-3-Vuex-SSR/nuxt-demo/ssr/pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+      <h1>{{ title }}</h1>
+      <ul v-for="item in posts" :key="item.id">
+          <li>{{ item.title }}</li>
+      </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+    name: 'Home',
+    // Nuxt中特殊提供的一个钩子函数，专门用于获取页面服务端渲染的数据
+    async asyncData () {
+        const { data } = await axios({
+            method: 'GET',
+            url: 'http://localhost:3000/data.json'
+        })
+        // 返回一个对象 
+        return data
+    }
+}
+</script>
+
+<style>
+
+</style>
